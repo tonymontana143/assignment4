501d5b3e673ed45c42b3daa9aa466c5e0de18fdc
TonyMontana143 <221074@astanait.edu.kz>
2023-05-15 01:02:22 +0600

    test methods

--- src/MyTestingClass.java
+++ src/MyTestingClass.java
@@ -35,7 +35,7 @@ public class MyTestingClass {
         return age == other.age;
     }
 
-    // Example usage of MyTestingClass with String as the value type
+    // Example usage of MyTestingClass with MyHashTable
     public static void main(String[] args) {
         MyHashTable<MyTestingClass, String> table = new MyHashTable<>();
         Random random = new Random();
@@ -54,5 +54,23 @@ public class MyTestingClass {
         for (int i = 0; i < bucketSizes.length; i++) {
             System.out.println("Bucket " + i + ": " + bucketSizes[i] + " elements");
         }
+
+        // Get a value using a key
+        MyTestingClass searchKey = new MyTestingClass("Name5", 20);
+        String retrievedValue = table.get(searchKey);
+        System.out.println("Retrieved value: " + retrievedValue);
+
+        // Remove a key-value pair
+        MyTestingClass removeKey = new MyTestingClass("Name10", 30);
+        String removedValue = table.remove(removeKey);
+        System.out.println("Removed value: " + removedValue);
+
+        // Check if a value exists in the table
+        boolean containsValue = table.contains("Value100");
+        System.out.println("Contains value 'Value100': " + containsValue);
+
+        // Get the key for a value
+        MyTestingClass keyForValue = table.getKey("Value500");
+        System.out.println("Key for value 'Value500': " + keyForValue);
     }
 }
