f0a74aa6c07e0855fee007647db91c92dd2b694f
TonyMontana143 <221074@astanait.edu.kz>
2023-05-14 16:04:30 +0600

    Method "put" written, to insert  new element

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -37,6 +37,26 @@ public class MyHashTable<K, V> {
     }
 
     public void put(K key, V value) {
+        int index = hash(key);
+        HashNode<K, V> newNode = new HashNode<>(key, value);
+        if (chainArray[index] == null) {
+            chainArray[index] = newNode;
+        } else {
+            HashNode<K, V> currentNode = chainArray[index];
+            while (currentNode.next != null) {
+                if (currentNode.key.equals(key)) {
+                    currentNode.value = value;
+                    return;
+                }
+                currentNode = currentNode.next;
+            }
+            if (currentNode.key.equals(key)) {
+                currentNode.value = value;
+            } else {
+                currentNode.next = newNode;
+            }
+        }
+        size++;
     }
 
     public V get(K key) {
